aac447d8d3bfac62cada741c18154f2175ccc9c4
fredoot <alfredotoselli0@gmail.com>
2022-12-05 16:40:00 +0100

    pre-commit test 2

--- fractals_app/apps.py
+++ fractals_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FractalsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fractals_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "fractals_app"
